76dfbcf513f922e154f37955de8601376ffc88c7
Krystian Florek on Linux <krystiankrk@gmail.com>
2024-11-13 21:24:14 +0100

    4. Komentarze

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
+// Biblioteka zawierajaca funkcje wejscia wyjscia
 #include <iostream>
 
 int main()
 {
+    // Komenda wyswietlajaca w konsoli slowo "Hello World!"
     std::cout << "Hello World! " << std::endl;
-
+    /*
     std::cout << "Number1";
     std::cout << "Number2" << std::endl;
+    */
 
     return 0;
+    // Tu program sie konczy.
 }
