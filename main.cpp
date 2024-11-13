94f2b53113d384567579839dfc9ab8bcb8eb4c6b
Krystian Florek on Linux <krystiankrk@gmail.com>
2024-11-13 21:21:40 +0100

    3. cout Hello World!

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 
-consteval int get_value()
-{
-    return 3;
-}
-
 int main()
 {
-    constexpr int value = get_value();
-    std::cout << "value : " << value << std::endl;
+    std::cout << "Hello World! " << std::endl;
+
+    std::cout << "Number1";
+    std::cout << "Number2" << std::endl;
+
     return 0;
 }
