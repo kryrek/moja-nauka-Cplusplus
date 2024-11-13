44a04f0bb7c3862330266972721f3da964aa7087
Krystian Florek on Linux <krystiankrk@gmail.com>
2024-11-13 21:31:37 +0100

    5. Rodzaje bledow

--- main.cpp
+++ main.cpp
@@ -10,6 +10,14 @@ int main()
     std::cout << "Number2" << std::endl;
     */
 
+    // Rodzaje bledow
+    // Compile Time Error - brak srednika, program sie nieskompiluje
+    // std::cout << "Hello World! " << std::endl
+
+    // Run Time Error - dzielenie przez zero, program sie skompiluje
+    int value = 7 / 0;
+    std::cout << "Value: " << value << std::endl;
+
     return 0;
     // Tu program sie konczy.
 }
